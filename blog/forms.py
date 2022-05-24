e9c92e47cd7ed0542769ab15fe0c081019d82e07
baaserosman <baaser.osman@gmail.com>
2022-05-24 16:39:44 +0200

    views, urls, templates added

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Post, Comment, Category
 
+
 class PostForm(forms.ModelForm):
     status = forms.ChoiceField(choices=Post.OPTIONS)
     category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Select")
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+# from .forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm
 
-# Create your views here.
+
+def post_list(request):
+    qs = Post.objects.filter(status='p')
+    context = {
+        'object_list' : qs
+    }
+    return render (request, "blog/post_list.html", context )
+
+def post_create(request):
+    # form = PostForm(request.POST or None, request.FILES or None)
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        # print(request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            # messages.success(request, "Post created successfully!")
+            return redirect("blog:list")
+    context = {
+        'form' : form
+    }
+    return render (request, "blog/post_create.html", context)
