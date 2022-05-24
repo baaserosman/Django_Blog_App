3744e0481aff83869ee25ee2fd386e5f783f3d37
baaserosman <baaser.osman@gmail.com>
2022-05-24 15:44:28 +0200

    media added

--- main/settings.py
+++ main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8=xlwky8)y7h&$-g@216jsud+x_xb%1=+%1p-o8nz7)tx$2k@1'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
